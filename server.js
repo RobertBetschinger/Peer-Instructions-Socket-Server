be8509b44091021e9539e2edd7c8bab42ca7ec6b
Robert Betschinger <robert-betschinger@outlook.de>
2020-05-01 12:02:15 +0200

    Added Private Messages

--- server.js
+++ server.js
@@ -1,83 +1,139 @@
-const express = require('express')
-const socket = require('socket.io')
-const http = require('http')
+const express = require("express");
+const socket = require("socket.io");
+const http = require("http");
 const app = express();
 const server = http.createServer(app);
 const io = socket(server);
-const cors = require('cors');
+const cors = require("cors");
 
-const router = require('./router');
-const PORT = process.env.PORT || 5000
+const router = require("./router");
+const PORT = process.env.PORT || 5000;
 
-const users ={}
-const data ={
+const users = [];
+const data = {
   ant1: 0,
   ant2: 0,
   ant3: 0,
-  ant4: 0
-}
+  ant4: 0,
+};
 
-io.on('connection', function(socket){
-          console.log('a user connected');
-        	socket.on('disconnect', function(){
-                 console.log('user disconnected');
-                 socket.broadcast.emit('Person Disconnected',users[socket.id])
-                 delete users[socket.id]
-          });
-           
+io.on("connection", function (socket) {
+  console.log("a user connected");
+
+  socket.on("disconnect", function () {
+    console.log("user disconnected");
+    let disconnectedName;
+    for (let i = 0; i < users.length; i++) {
+      if (users[i].id === socket.id) {
+        disconnectedName = users[i].userName;
+        users.splice(i, 1);
+      }
+    }
+    socket.broadcast.emit("Group-Changed", users);
+    socket.broadcast.emit("Person Disconnected", disconnectedName);
+  });
+
+  socket.on("newPhase", (message) => {
+    socket.broadcast.emit("newPhase", message);
+  });
+
+  socket.on("send-chat-message", (message) => {
+    let userName;
+    for (let i = 0; i < users.length; i++) {
+      if (users[i].id === socket.id) {
+        userName = users[i];
+      }
+    }
+
+    socket.broadcast.emit("chat-message", {
+      message: message,
+      name: userName.userName,
+    });
+  });
+
+  socket.on("New User", (name) => {
+    users.push({
+      id: socket.id,
+      userName: name,
+    });
+
+    console.log(users);
+    socket.broadcast.emit("Person joined", name);
+    io.sockets.emit("Group-Changed", users);
+  });
+
+  socket.on("Private-Message", (data,answer) => {
+    if(data.id === socket.id){
+      console.log("Tryed to send at yourself")
+      answer(true)
+      return
+    }
+    answer(false)
+    let userName;
+    for (let i = 0; i < users.length; i++) {
+      if (users[i].id === socket.id) {
+        userName = users[i];
+      }
+    }
+
+   
 
 
-            socket.on('newPhase', message =>{
-              socket.broadcast.emit('newPhase', message)
-            })
 
-            socket.on('send-chat-message', message => {
-              socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
-            })
+    const privMessageObj ={
+      message: data.message,
+      name: userName.userName
+    }
+    console.log(privMessageObj.message)
+    console.log(privMessageObj.name)
+    socket.broadcast.to(data.id).emit("private-message", privMessageObj);
+    console.log("Private Message Sucessfully")
+  });
 
-            socket.on('New User', name =>{
-              users[socket.id] = name
-              socket.broadcast.emit('Person joined', name)
-            })
+  socket.on("NewQuestion", (question) => {
+    socket.broadcast.emit("NewQuestion", question);
+    data.ant1 = 0;
+    data.ant2 = 0;
+    data.ant3 = 0;
+    data.ant4 = 0;
+  });
 
-            socket.on('NewQuestion',question =>{
-              socket.broadcast.emit('NewQuestion', question)
-              data.ant1=0;
-              data.ant2=0;
-              data.ant3=0;
-              data.ant4=0;
-            })
+  //Only Broadcast to Ludwig Englbrecht
+  socket.on("question-answered", (value) => {
+    if (value == 1) {
+      data.ant1++;
+    }
+    if (value == 2) {
+      data.ant2++;
+    }
+    if (value == 3) {
+      data.ant3++;
+    }
+    if (value == 4) {
+      data.ant4++;
+    }
 
-            //Only Broadcast to Ludwig Englbrecht
-            socket.on('question-answered',value =>{
-              if(value ==1){
-                data.ant1++
-              }
-            if(value ==2){
-              data.ant2++
-            }
-            if(value ==3){
-              data.ant3++
-            }
-            if(value ==4){
-              data.ant4++
-            }
-            const PersonData ={
-              name:users[socket.id],
-              value:value
-            }
-           
-              socket.broadcast.emit('question-answered',PersonData)
-            })
+    let userName;
+    for (let i = 0; i < users.length; i++) {
+      if (users[i].id === socket.id) {
+        userName = users[i];
+      }
+    }
+    const PersonData = {
+      name: userName.userName,
+      value: value,
+    };
 
-            socket.on('showStatistics', function(){
-              console.log("data arrvied at server");
-              socket.broadcast.emit('showStatistics',data)
-            })
+    socket.broadcast.emit("question-answered", PersonData);
+  });
+
+  socket.on("showStatistics", function () {
+    console.log("data arrvied at server");
+    socket.broadcast.emit("showStatistics", data);
+  });
 });
 
 //This Part has to be at the bottom of the Code
 app.use(router);
-app.use(cors())
-server.listen(PORT, () => console.log('Server has started on Port: ' +  PORT));
-
+app.use(cors());
+server.listen(PORT, () => console.log("Server has started on Port: " + PORT));
